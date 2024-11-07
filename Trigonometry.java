89bc05b18f2f70b6e9bb027e442a9859a36286d0
Siddhi Satam <144619023+DoitSid@users.noreply.github.com>
2024-11-07 20:32:04 +0530

    Add files via upload

---
+++ Trigonometry.java
@@ -0,0 +1,28 @@
+package Mathematics;
+
+public class Trigonometry {
+    // Sine of an angle (in radians)
+    public static double sine(double angle) {
+        return Math.sin(angle);
+    }
+
+    // Cosine of an angle (in radians)
+    public static double cosine(double angle) {
+        return Math.cos(angle);
+    }
+
+    // Tangent of an angle (in radians)
+    public static double tangent(double angle) {
+        return Math.tan(angle);
+    }
+
+    // Converts degrees to radians
+    public static double toRadians(double degrees) {
+        return Math.toRadians(degrees);
+    }
+
+    // Converts radians to degrees
+    public static double toDegrees(double radians) {
+        return Math.toDegrees(radians);
+    }
+}
